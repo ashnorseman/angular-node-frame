90494e7612c60da40f729fdd6480b017a4803d13
Ash Norseman <ash.norseman@gmail.com>
2014-04-22 17:30:21 +0800

    Tab controls

--- app/scripts/controllers/content.js
+++ app/scripts/controllers/content.js
@@ -3,8 +3,5 @@
 angular.module('angularNodeFrameApp')
   .controller('ContentCtrl', function ($scope) {
 
-    $scope.$on("changeContent", function (event, data) {
-      $scope.title = data.showName;
-      $scope.uri = data.uri;
-    });
+
   });
--- app/scripts/controllers/side-nav.js
+++ app/scripts/controllers/side-nav.js
@@ -3,13 +3,33 @@
 angular.module('angularNodeFrameApp')
   .controller('SideNavCtrl', function ($rootScope, $scope, Menu) {
 
+    function deActivate(tab) {
+      tab.active = false;
+    }
+
+    $rootScope.tabs = [];
+
     $scope.menu = Menu.query();
 
-    $scope.openMenu = function (menuName) {
-      Menu.get({
-        name: menuName
-      }, function (data) {
-        $rootScope.$broadcast("changeContent", data);
-      });
+    $rootScope.openMenu = function (menuName) {
+
+      var oldTab = $rootScope.tabs.filter(function (tab) {
+        if (tab.name === menuName) {
+          return true;
+        }
+      })[0];
+
+      if (oldTab) {
+        $rootScope.tabs.forEach(deActivate);
+        oldTab.active = true;
+      } else {
+        Menu.get({
+          name: menuName
+        }, function (newTab) {
+          newTab.active = true;
+          $rootScope.tabs.forEach(deActivate);
+          $rootScope.tabs.push(newTab);
+        });
+      }
     }
   });
