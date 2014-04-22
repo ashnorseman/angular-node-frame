a337f83335929ffcf10e4d8234347685f6370f10
Ash Norseman <ash.norseman@gmail.com>
2014-04-22 16:43:28 +0800

    Now can open menus

--- app/scripts/controllers/content.js
+++ app/scripts/controllers/content.js
@@ -3,4 +3,8 @@
 angular.module('angularNodeFrameApp')
   .controller('ContentCtrl', function ($scope) {
 
+    $scope.$on("changeContent", function (event, data) {
+      $scope.title = data.showName;
+      $scope.uri = data.uri;
+    });
   });
--- app/scripts/controllers/side-nav.js
+++ app/scripts/controllers/side-nav.js
@@ -1,7 +1,15 @@
 'use strict';
 
 angular.module('angularNodeFrameApp')
-  .controller('SideNavCtrl', function ($scope, Menu) {
+  .controller('SideNavCtrl', function ($rootScope, $scope, Menu) {
 
     $scope.menu = Menu.query();
+
+    $scope.openMenu = function (menuName) {
+      Menu.get({
+        name: menuName
+      }, function (data) {
+        $rootScope.$broadcast("changeContent", data);
+      });
+    }
   });
